34323a7bb3a4a73d3a5818f7b986a1c8187fedb9
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2019-09-12 23:13:30 -0300

    Criado um model chamado 'Anuncio' e alterado o arquivo 'admin.py'.

--- anuncio/app_anuncio/admin.py
+++ anuncio/app_anuncio/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+class AnuncioAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'Titulo', 'preco', 'Anuncio',
+                    'nome_contato', 'telefone', 'secao',
+                    'tipo_anuncio')
+
+
+admin.site.register(Anuncio,AnuncioAdmin)
+
+
+
--- anuncio/app_anuncio/models.py
+++ anuncio/app_anuncio/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Anuncio(models.Model):
+    cliente = models.CharField(max_length=50)
+    Titulo = models.CharField(max_length=80)
+    preco = models.DecimalField(decimal_places=2,max_digits=2)
+    Anuncio = models.TextField()
+    nome_contato = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=10)
+    secao = models.CharField(max_length=10)
+    tipo_anuncio = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Titulo
